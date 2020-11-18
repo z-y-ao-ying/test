e398e27cee4b1c7f59eee30b0e906588b21adc88
test <test@163.com>
2020-11-18 13:49:38 +0800

    hi

--- helloworld/src/test/HelloWorld.java
+++ helloworld/src/test/HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world.......");
 	}
+	public void hi() {
+		System.out.println("hi..........");
+	}
 }
